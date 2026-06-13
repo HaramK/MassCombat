c02cafd153786d6f304fa7048e6bc9c606858db3
HaramK <37320983+HaramK@users.noreply.github.com>
2026-06-13 17:31:19 +0900

    [Setup] add C++ source module + project setup

---
+++ Source/MassCombat/MCTestClass.h
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+/**
+ * 
+ */
+class MASSCOMBAT_API MCTestClass
+{
+public:
+	MCTestClass();
+	~MCTestClass();
+};
